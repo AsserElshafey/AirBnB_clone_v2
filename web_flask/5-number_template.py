ee6f67b516b8a0cebda64d3ba6ed4fc69796019f
Asser Elshafey <elshafeyasser@yahoo.com>
2023-10-21 23:01:54 +0300

    numbers

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,7 +37,7 @@ def isanumber(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_page(n):
+def numberPage(n):
     """returning an html template"""
     return render_template('5-number.html', n=n)
 
